c0d145277576974a2fc5a1b3cc1f3576e9dfd1e2
MBATheGamer <mbathegamer@gmail.com>
2024-06-07 19:31:21 +0100

    Lesson

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,7 +1,18 @@
 <script setup>
+import { useRouter } from "vue-router";
+
 const props = defineProps(["event"]);
+
+const router = useRouter();
+
+const register = () => {
+  router.push({
+    name: "EventDetails",
+  });
+};
 </script>
 
 <template>
   <p>Register for event here</p>
+  <button @click="register">Register Me!</button>
 </template>
